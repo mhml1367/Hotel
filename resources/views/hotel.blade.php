729b33e0192f1d9d3e0bed754004595b1ea69d90
mhml <mhml1367@gmail.com>
2020-02-27 14:40:37 +0330

    Update

---
+++ resources/views/hotel.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fa">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>رزرو بوم گردی</title>
+        <link rel="stylesheet" href="/asset/css/style.css">
+        <link rel="stylesheet" href="/asset/css/bootstrap.min.css">
+
+    </head>
+    <body class="bg-light">
+        <div class="container">
+            <div class="py-5 text-center">
+                <img class="d-block mx-auto mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+                    <h2>لاراول</h2>
+                <p class="lead">لاراوا</p>
+            </div>
+
+        </div>
+
+        <div class="container">
+            <div class="card-deck mb-3 text-center">
+
+
+              <div class="card mb-4 box-shadow">
+                <div class="card-header">
+                  <h4 class="my-0 font-weight-normal">Free</h4>
+                </div>
+                <div class="card-body">
+                  <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
+           
+                  <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+                </div>
+              </div>
+
+
+
+            </div>
+          </div>
+
+
+        <script src="/asset/js/jquery.min.js"></script>
+        <script>
+
+
+
+
+        </script>
+    </body>
+</html>
